e150e782b389579409ea93458428087acb6b3946
sizukutamago <b5241@oic.jp>
2016-10-09 02:54:30 +0900

    Initial Commit

---
+++ jancode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jancode
+//
+//  Created by sizukutamago on 2016/10/09.
+//  Copyright © 2016年 sizukutamago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
